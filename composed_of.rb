5f7bcdaa7b10d0d1242e1a06f006b821c9564d70
root <root@cn28>
2016-07-13 23:13:31 +0800

    rails : active record composed of

--- composed_of.rb
+++ composed_of.rb
@@ -1,10 +1,14 @@
+# composed_of : add in the model class , model class relates with a table , abstract several columns in the table into a class.
+# model class : Cloud
+# table : clouds
+# abstract class : Combo
 require "rubygems"
 require "active_record"
 puts ActiveRecord::Base.establish_connection(:adapter => "mysql", :host => "localhost", :database => "hycloud_portal")
 class Cloud < ActiveRecord::Base
-  composed_of :combo,
-              :class_name => "Combo",
-              :mapping => [[:name,:name],[:url,:url]]
+  composed_of :combo,	# model class's attribute , named combo
+              :class_name => "Combo",	# abstract class , named Combo
+              :mapping => [[:name,:name],[:url,:url]]	# Mapping relation => [:column_name,:attr_name]
 end
 
 class Combo
