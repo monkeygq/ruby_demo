6deec461bb8dee645d238dae6bc8c410acd365c3
root <root@cn28>
2016-07-18 14:02:19 +0800

    ruby : keyword : self

--- self.rb
+++ self.rb
@@ -4,9 +4,9 @@ p self	#=> main
 p self.class	#=> Object
 
 def mymethod	
-	p self	#=> main
-	@flag2 = self
-	p @flag1 == @flag2	#=> true
+  p self	#=> main
+  @flag2 = self
+  p @flag1 == @flag2	#=> true
 end
 
 mymethod
@@ -14,13 +14,13 @@ mymethod
 puts "#===================================================>"
 
 class Myclass
-	p self	#=> Myclass
-	def instance_method
-		p self	#=> #<Myclass:0x00000001df3f30> : an instance
-	end
-	def self.class_method
-		p self	#=> Myclass
-	end
+  p self	#=> Myclass
+  def instance_method
+    p self	#=> #<Myclass:0x00000001df3f30> : an instance
+  end
+  def self.class_method
+    p self	#=> Myclass
+  end
 end
 
 Myclass.new.instance_method
@@ -29,11 +29,11 @@ Myclass.class_method
 puts "#===================================================>"
 
 class Person
-	attr_accessor :name
-	def set_name(para)
-	  @name = para
-		#self.name = para	# @name is same to self.name
-	end
+  attr_accessor :name
+  def set_name(para)
+    @name = para
+    #self.name = para	# @name is same to self.name
+  end
 end
 
 body = Person.new
