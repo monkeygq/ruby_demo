89edf84d6a6a74293f93f5b661b3ea96e7a6d958
monkeygq <hgq6389465@gmail.com>
2016-07-25 11:42:14 +0800

    ruby : test unit

---
+++ test_unit.rb
@@ -0,0 +1,37 @@
+require "test/unit"
+class Myclass
+  def initialize(a, b)
+    @a = a
+    @b = b
+  end 
+  def mymethod
+    c, d = @a.divmod @b
+    c
+  end
+end
+
+class TestMyclass < Test::Unit::TestCase
+  def test_mymethod # method name must begin with test
+    assert_equal(3, Myclass.new(100, 30).mymethod)
+    assert_equal(4, Myclass.new(100, 30).mymethod)
+  end
+end
+=begin
+
+Run options: 
+
+# Running tests:
+
+F
+
+Finished tests in 0.000743s, 1346.7052 tests/s, 2693.4104 assertions/s.
+
+  1) Failure:
+test_mymethod(TestMyclass) [myclass.rb:16]:
+<4> expected but was
+<3>.
+
+1 tests, 2 assertions, 1 failures, 0 errors, 0 skips
+
+=end
+
