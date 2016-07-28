4248232a29b2c4127c67d6895b0b1b200a74e3aa
monkeygq <hgq6389465@gmail.com>
2016-07-28 22:47:47 +0800

    ruby : class or object extension for singleton class , class get new class method , object get new singleton method

--- class_extension_and_object_extension.rb
+++ class_extension_and_object_extension.rb
@@ -1,9 +1,11 @@
 #==================== class extension ====================>
+# class entension for the class's singleton class , the class gets new class methods
 module Mymodule
-  def mymethod
+  def mymethod  # define instance method
     "233"
   end
 end
+p Mymodule.instance_methods(false)  #=> [:mymethod]
 
 class Myclass
   class << self  # Myclass's singleton class's scope
@@ -11,12 +13,23 @@ class Myclass
   end
 end
 p Myclass.mymethod  #=> "233"
-p Myclass.methods(false)
+
+
+# class extension : advanced version
+class Myclass2
+  extend Mymodule
+end
+p Myclass.mymethod  #=> "233"
 
 #=================== object extension ====================>
-
+#object extension for the object's singleton class , the object gets new singleton methods
 o = Object.new
 class << o
   include Mymodule
 end
 p o.mymethod        #=> "233"
+
+#object extension : advanced version
+o2 = Object.new
+o2.extend Mymodule
+p o2.mymethod       #=> "233"
