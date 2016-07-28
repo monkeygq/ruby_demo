b55400cf76da9f702117ec54b9a3052aa9429379
monkeygq <hgq6389465@gmail.com>
2016-07-28 16:20:19 +0800

    ruby : change method name(not alias) , define_method and send (advanced version : self , receive and pass arguments , block)

--- change_method_name.rb
+++ change_method_name.rb
@@ -6,13 +6,18 @@ class Couple
     end
   end
   def self.change_method_name(new_name, old_name)
-    define_method(old_name) do |*args, &block|
+    #self is class
+    define_method(old_name) do |*args, &block|  # receice all arguments and block
       puts "#{old_name}() is deprecated. Use #{new_name}()."
-      send new_name, *args, &block
+      send new_name, *args, &block  # in block , self is class instance object , pass all arguments and block by send method
     end
   end
   change_method_name :new_love, :old_love
 end
 
 Couple.new.old_love("hgq", "lcx") { puts "have a block" }
+
+#=> old_love() is deprecated. Use new_love().
+#=> hgq and lcx are a couple
+#=> have a block
 
