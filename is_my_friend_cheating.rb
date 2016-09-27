ef75fd2bea96ce56d5267740e5d7eb2dc38caabd
monkeygq <hgq6389465@gmail.com>
2016-09-27 22:59:54 +0800

    ruby : codewars

--- is_my_friend_cheating.rb
+++ is_my_friend_cheating.rb
@@ -2,14 +2,16 @@
 # Within that sequence, he chooses two numbers, a and b.
 # He says that the product of a and b should be equal to the sum of all numbers in the sequence, excluding a and b.
 # Given a number n, could you tell me the numbers he excluded from the sequence?
-# The function takes the parameter: n (don't worry, n is always strictly greater than 0 and small enough so we shouldn't have overflow) and returns an array of the form:
+# The function takes the parameter: n (don't worry, n is always strictly greater than 0 and small enough so we shouldn't
+#  have overflow) and returns an array of the form:
 
 # [(a, b), ...] or [[a, b], ...] or {{a, b}, ...} or or [{a, b}, ...]
 # with all (a, b) which are the possible removed numbers in the sequence 1 to n.
 
 # [(a, b), ...] or [[a, b], ...] or {{a, b}, ...} or ...will be sorted in increasing order of the "a".
 
-# It happens that there are several possible (a, b). The function returns an empty array if no possible numbers are found which will prove that my friend has not told the truth!
+# It happens that there are several possible (a, b). The function returns an empty array if no possible numbers are found 
+# which will prove that my friend has not told the truth!
 
 # Examples:
 
