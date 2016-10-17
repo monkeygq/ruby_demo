2f776ba69ae44ec722507521d9dcd750e4d89d39
monkeygq <hgq6389465@gmail.com>
2016-10-17 22:08:28 +0800

    quicksort , The first k small/large number in the array

--- data_structures_and_algorithm_analysis/quicksort.rb
+++ data_structures_and_algorithm_analysis/quicksort.rb
@@ -12,43 +12,20 @@ def quicksort2(b)
 end
 b = [3,45,6,7,34,14,62,5,6,7,9,1,3,4,45,36]
 p quicksort2 b  #=> [62, 45, 45, 36, 34, 14, 9, 7, 7, 6, 6, 5, 4, 3, 3, 1]
+ 
+# [1, 3, 3, 4, 5, 6, 6, 7, 7, 9, 14, 34, 36, 45, 45, 62] 
+# [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 
 def firstk(a, k)
-  puts "------------------------------"
-  p "k=#{k}"
   x = a.pop
   small = a.select { |i| i <= x } 
-  p "small=#{small}"
-  p "x=#{x}"
   big =a.select { |i| i > x } 
-  p "big=#{big}"
-  puts "++++++++++++++++++++++++++++++"
-  if k == 0 && small.size == 0
+  if k == small.size + 1
     return x
   else
-    k < small.size ? firstk(small, k) : firstk([x] + big, k - small.size)
+    k <= small.size ? firstk(small, k) : firstk(big, k - small.size - 1)
   end 
 end
 
 c = [3,45,6,7,34,14,62,5,6,7,9,1,3,4,45,36]
-p firstk(c, 8)  #=> 7
-
-def firstk2(a, k)
-  puts "------------------------------"
-  p "k=#{k}"
-  x = a.pop
-  small = a.select { |i| i >= x } 
-  p "small=#{small}"
-  p "x=#{x}"
-  big =a.select { |i| i < x } 
-  p "big=#{big}"
-  puts "++++++++++++++++++++++++++++++"
-  if k == 0 && small.size == 0
-    return x
-  else
-    k < small.size ? firstk2(small, k) : firstk2([x] + big, k - small.size)
-  end
-end
-d = [3,45,6,7,34,14,62,5,6,7,9,1,3,4,45,36]
-p firstk2(d, 0) #=> 62
-
+p firstk c, 10  #=> 9
