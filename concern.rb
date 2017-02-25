9015924cef65b9404b6bed5494a7d74cadd07e25
monkeygq <hgq6389465@gmail.com>
2017-02-25 14:22:19 +0800

    rails : ActiveSupport::Concern

--- concern.rb
+++ concern.rb
@@ -17,3 +17,44 @@ end
 
 p Myclass.new.an_instance_method  #=> "an_instance_method"
 p Myclass.a_class_method  #=> "a_class_method"
+p Myclass.ancestors  #=> [Myclass, Myconcern, Object, Kernel, BasicObject]
+
+#======================================================================================>
+# include chain : Mytest < Myc1 < Myc2
+module Myc2
+  extend ActiveSupport::Concern
+  def m3
+    p "Myc2 instance method"
+  end 
+  module ClassMethods
+    def m4
+      p "Myc2 class method"
+    end 
+  end 
+end
+
+module Myc1
+  extend ActiveSupport::Concern
+  include Myc2
+  def m1
+    p "Myc1 instance method"
+  end 
+  module ClassMethods
+    def m2
+      p "Myc1 class method"
+    end 
+  end 
+end
+
+class Mytest
+  include Myc1
+end
+
+
+Mytest.m2 #=> "Myc1 class method"
+Mytest.m4 #=> "Myc2 class method"
+Mytest.new.m1 #=> "Myc1 instance method"
+Mytest.new.m3 #=> "Myc2 instance method"
+p Mytest.ancestors #=> [Mytest, Myc1, Myc2, Object, Kernel, BasicObject]
+
+
