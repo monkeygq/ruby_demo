8efd15196a43959baa07c38029bc6d1e7ee36a95
houguanqun <houguanqun@score.cool>
2017-03-17 13:18:53 +0800

    quicksort by Introduction to Algorithms

---
+++ data_structures_and_algorithm_analysis/quicksort_standard.rb
@@ -0,0 +1,22 @@
+def partition(a, p ,r)
+  i = p - 1
+  for j in p..(r-1)
+    if a[j] < a[r]
+      i = i + 1
+      a[i], a[j] = a[j], a[i]
+    end
+  end
+  a[i+1], a[r] = a[r], a[i+1]
+  return i + 1
+end
+def quicksort(a, p ,r)
+  if p < r
+    q = partition(a, p ,r)
+    quicksort(a, p, q-1)
+    quicksort(a, q+1, r)
+  end
+end
+
+a = [3,5,9,7,4,2,8,7]
+quicksort(a, 0, 7)
+p a #=> [2, 3, 4, 5, 7, 7, 8, 9]
