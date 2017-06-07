6a57b5ea891695f575c8d168680401e8c5a0b99d
monkeygq <hgq6389465@gmail.com>
2017-06-08 00:22:06 +0800

    ruby : for lcx

---
+++ calc.rb
@@ -0,0 +1,79 @@
+require 'spreadsheet'
+Spreadsheet.client_encoding = 'UTF-8'
+book = Spreadsheet.open 'mylcx.xls'
+sheet = book.worksheet('Sheet1')
+block_count = sheet.row(0).length - 1
+h = {}
+gravity = {}
+block_count.times do |index|
+  h["s#{index}"] = {}
+  gravity["s#{index}"] = {}
+end
+
+
+p sheet.row(0)
+p "+++++++++++++++++++++++++++++++++++++++++++++++++++=="
+sheet.each_with_index do |row, index|
+  row.each do |item|
+    print "%10s" % "#{item}"
+  end
+  puts
+  if index > 0
+    row.each_with_index do |item, col|
+      if col > index
+        h[row[0]]["s#{col-1}"] = item.to_f if item
+      end
+    end
+  end
+end
+
+#p h 
+
+arr = [3,6,4,7,4,9]
+area = {}
+
+arr.each_with_index do |i, index|
+  area["s#{index}"] = i.to_f
+end
+
+print "area="
+p area
+
+h.each do |k1, v1|
+   v1.each do |k2, v2|
+     gravity[k1][k2] = area[k1] * area[k2] / (h[k1][k2] ** 2)
+   end
+end
+
+print "gravity="
+p gravity
+sort_arr = []
+gravity.each do |k1, v1|
+  v1.each do |k2, v2|
+    sort_arr << [k1, k2, v2]
+  end
+end
+
+p sort_arr.sort { |a, b| b[2] <=> a[2] }
+
+new_book = Spreadsheet::Workbook.new
+new_sheet = new_book.create_worksheet
+
+new_row_0 = []
+
+block_count.times do |index|
+  new_row_0 << "s#{index}"
+end
+p new_sheet.row(0).concat([nil] + new_row_0 )
+
+block_count.times do |index1|
+  k1 = "s#{index1}"
+  new_sheet.row(index1+1).concat([k1])
+  block_count.times do |index2|
+    k2 = "s#{index2}"
+    value = gravity[k1][k2] ? gravity[k1][k2] : nil
+    new_sheet.row(index1+1).concat([value])
+  end
+end
+
+new_book.write './result.xls'
