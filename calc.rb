35c0ca1013cb47075d74ff9dff5a2caeb70eedb9
monkeygq <hgq6389465@gmail.com>
2017-06-24 22:12:53 +0800

    ruby : for lcx v2.0

--- calc.rb
+++ calc.rb
@@ -1,27 +1,28 @@
 require 'spreadsheet'
 Spreadsheet.client_encoding = 'UTF-8'
-book = Spreadsheet.open 'mylcx.xls'
-sheet = book.worksheet('Sheet1')
+book = Spreadsheet.open 'gravity.xls'
+sheet = book.worksheet('sheet2')
 block_count = sheet.row(0).length - 1
 h = {}
 gravity = {}
 block_count.times do |index|
-  h["s#{index}"] = {}
-  gravity["s#{index}"] = {}
+  h["S#{index}"] = {}
+  gravity["S#{index}"] = {}
 end
 
+#p h
 
-p sheet.row(0)
-p "+++++++++++++++++++++++++++++++++++++++++++++++++++=="
+#p sheet.row(0)
+#p "+++++++++++++++++++++++++++++++++++++++++++++++++++=="
 sheet.each_with_index do |row, index|
-  row.each do |item|
-    print "%10s" % "#{item}"
-  end
-  puts
+  #row.each do |item|
+  #  print "%10s" % "#{item}"
+  #end
+  #puts
   if index > 0
     row.each_with_index do |item, col|
       if col > index
-        h[row[0]]["s#{col-1}"] = item.to_f if item
+        h[row[0]]["S#{col-1}"] = item.to_f if item
       end
     end
   end
@@ -29,15 +30,16 @@ end
 
 #p h 
 
-arr = [3,6,4,7,4,9]
+
+arr = book.worksheet('sheet1').row(1)
 area = {}
 
 arr.each_with_index do |i, index|
-  area["s#{index}"] = i.to_f
+  area["S#{index}"] = i.to_f
 end
 
-print "area="
-p area
+#print "area="
+#p area
 
 h.each do |k1, v1|
    v1.each do |k2, v2|
@@ -45,35 +47,50 @@ h.each do |k1, v1|
    end
 end
 
-print "gravity="
-p gravity
+#print "gravity="
+#p gravity
 sort_arr = []
 gravity.each do |k1, v1|
   v1.each do |k2, v2|
     sort_arr << [k1, k2, v2]
   end
 end
-
-p sort_arr.sort { |a, b| b[2] <=> a[2] }
-
+#p "++++++++++++++++++= gravity first 1128++++++++++++++++=="
+output = []
+hash = Hash.new(0)
+sort_arr.sort { |a, b| b[2] <=> a[2] }.first(132).map do |i|
+  #print "%04s" % "#{index + 1}"
+  #print ". "
+  #print i
+  #puts
+  #output << [index+1, i.last]
+  #p i
+  hash[i[0]] += 1
+  hash[i[1]] += 1
+end
+p hash.to_a.sort { |a, b| a[1] <=> b[1] }
+p hash.count
+#p output
+=begin
 new_book = Spreadsheet::Workbook.new
 new_sheet = new_book.create_worksheet
 
 new_row_0 = []
 
 block_count.times do |index|
-  new_row_0 << "s#{index}"
+  new_row_0 << "S#{index}"
 end
-p new_sheet.row(0).concat([nil] + new_row_0 )
+new_sheet.row(0).concat([nil] + new_row_0 )
 
 block_count.times do |index1|
-  k1 = "s#{index1}"
+  k1 = "S#{index1}"
   new_sheet.row(index1+1).concat([k1])
   block_count.times do |index2|
-    k2 = "s#{index2}"
+    k2 = "S#{index2}"
     value = gravity[k1][k2] ? gravity[k1][k2] : nil
     new_sheet.row(index1+1).concat([value])
   end
 end
 
 new_book.write './result.xls'
+=end
